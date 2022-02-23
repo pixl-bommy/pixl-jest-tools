0e045933d5f7163125586807068d073ec62cda6e
Thomas Borck <git@bommy.net>
2022-02-23 22:11:33 +0100

    refactored printTestFooter to have fallback values

--- src/printTestFooter.ts
+++ src/printTestFooter.ts
@@ -1,17 +1,27 @@
 import { AggregatedResult } from "@jest/reporters";
 
-export default function printTestFooter(results: AggregatedResult): void {
-    const padLen = ("" + results.numTotalTests).length;
+export default function printTestFooter(
+    results: AggregatedResult,
+    now = Date.now()
+): void {
+    const numTotalTests = results.numTotalTests || 0;
+    const numPassedTests = results.numPassedTests || 0;
+    const numFailedTests = results.numFailedTests || 0;
+    const numPendingTests = results.numPendingTests || 0;
+    const numTodoTests = results.numTodoTests || 0;
+    const startTime = results.startTime || 0;
 
-    const end = new Date().valueOf();
-    const start = new Date(results.startTime).valueOf();
+    const padLen = ("" + numTotalTests).length;
+
+    const end = now;
+    const start = startTime;
     const seconds = (end - start) / 1000;
 
-    console.log(`Ran ${results.numTotalTests} tests in ${seconds} s`);
-    console.log(`  ${pad(padLen, results.numPassedTests || 0)} passing`);
-    console.log(`  ${pad(padLen, results.numFailedTests || 0)} failing`);
-    console.log(`  ${pad(padLen, results.numPendingTests || 0)} pending`);
-    console.log(`  ${pad(padLen, results.numTodoTests || 0)} todo`);
+    console.log(`Ran ${numTotalTests} tests in ${seconds} s`);
+    console.log(`  ${pad(padLen, numPassedTests)} passing`);
+    console.log(`  ${pad(padLen, numFailedTests)} failing`);
+    console.log(`  ${pad(padLen, numPendingTests)} pending`);
+    console.log(`  ${pad(padLen, numTodoTests)} todo`);
     console.log();
 }
 
